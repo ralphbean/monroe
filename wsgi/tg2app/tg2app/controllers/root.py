1d9e803dcea944b3e1a5f94757fe89041d46e797
Ralph Bean <ralph.bean@gmail.com>
2011-09-16 21:48:54 -0400

    Basic grantors chart.

--- wsgi/tg2app/tg2app/controllers/root.py
+++ wsgi/tg2app/tg2app/controllers/root.py
@@ -15,6 +15,7 @@ from tg2app.controllers.secure import SecureController
 from tg2app.controllers.error import ErrorController
 
 from tg2app.widgets import ForeclosureGrid
+import tw2.jit
 
 __all__ = ['RootController']
 
@@ -50,73 +51,40 @@ class RootController(BaseController):
 
     @expose('tg2app.templates.widget')
     def grid(self):
-        return dict(page='whut', widget=ForeclosureGrid)
+        return dict(widget=ForeclosureGrid)
 
+    @expose('tg2app.templates.widget')
+    def grantor(self, top=5):
+        try:
+            top = int(top)
+        except TypeError:
+            redirect('/grantor')
 
+        closures = model.Foreclosure.query.all()
 
-    @expose('tg2app.templates.about')
-    def about(self):
-        """Handle the 'about' page."""
-        return dict(page='about')
+        bucket = {}
+        for c in closures:
+            bucket[c.grantor] = bucket.get(c.grantor, 0) + 1
 
-    @expose('tg2app.templates.environ')
-    def environ(self):
-        """This method showcases TG's access to the wsgi environment."""
-        return dict(environment=request.environ)
+        items = bucket.items()
+        items.sort(lambda a, b: cmp(b[1], a[1]))
 
-    @expose('tg2app.templates.data')
-    @expose('json')
-    def data(self, **kw):
-        """This method showcases how you can use the same controller for a data page and a display page"""
-        return dict(params=kw)
+        bucket = dict(items[:top])
+        bucket['OTHER'] = sum([item[1] for item in items[top:]])
 
-    @expose('tg2app.templates.authentication')
-    def auth(self):
-        """Display some information about auth* on this application."""
-        return dict(page='auth')
+        p_data = {
+            'labels' : ['Foreclosures'],
+            'values' : [
+                {
+                    'label': key,
+                    'values': [value],
+                } for key, value in bucket.iteritems()
+            ]
+        }
 
-    @expose('tg2app.templates.index')
-    @require(predicates.has_permission('manage', msg=l_('Only for managers')))
-    def manage_permission_only(self, **kw):
-        """Illustrate how a page for managers only works."""
-        return dict(page='managers stuff')
+        class pie(tw2.jit.PieChart):
+            id = 'foreclosure_pie'
+            data = p_data
+            sliceOffset = 5
 
-    @expose('tg2app.templates.index')
-    @require(predicates.is_user('editor', msg=l_('Only for the editor')))
-    def editor_user_only(self, **kw):
-        """Illustrate how a page exclusive for the editor works."""
-        return dict(page='editor stuff')
-
-    @expose('tg2app.templates.login')
-    def login(self, came_from=url('/')):
-        """Start the user login."""
-        login_counter = request.environ['repoze.who.logins']
-        if login_counter > 0:
-            flash(_('Wrong credentials'), 'warning')
-        return dict(page='login', login_counter=str(login_counter),
-                    came_from=came_from)
-
-    @expose()
-    def post_login(self, came_from='/'):
-        """
-        Redirect the user to the initially requested page on successful
-        authentication or redirect her back to the login page if login failed.
-
-        """
-        if not request.identity:
-            login_counter = request.environ['repoze.who.logins'] + 1
-            redirect('/login',
-                params=dict(came_from=came_from, __logins=login_counter))
-        userid = request.identity['repoze.who.userid']
-        flash(_('Welcome back, %s!') % userid)
-        redirect(came_from)
-
-    @expose()
-    def post_logout(self, came_from=url('/')):
-        """
-        Redirect the user to the initially requested page on logout and say
-        goodbye as well.
-
-        """
-        flash(_('We hope to see you soon!'))
-        redirect(came_from)
+        return dict(widget=pie)
